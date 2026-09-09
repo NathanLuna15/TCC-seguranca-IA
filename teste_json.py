f066c94e6be92f94e0ac8bd3b6b8c5005167ad73
NATHAN PROENÇA DE LUNA <25205657@cfp127aluno.local>
2026-09-09 15:34:35 -0300

    script python para alterar a imagen atraves do terminal

--- teste_json.py
+++ teste_json.py
@@ -1,16 +1,36 @@
 from ultralytics import YOLO
+import json
+import sys
+
+# Verifica se foi informada uma imagem
+if len(sys.argv) < 2:
+    print("Uso: python teste_json.py nome_da_imagem.jpg")
+    exit()
+
+# Pega o nome da imagem
+imagem = sys.argv[1]
 
 #carregar o modelo trinado 
-model = YOLO("runs\detect\train-ppe-50\weights")
+model = YOLO("runs/detect/train-ppe-50/weights/best.pt")
 
 #analiza a imagem
-results = model("operador.jpg")
+results = model("barbudoSemCapacete.png")
 
 #pega o resultado
 resultado = results[0]
 
 #converte as detecção para JSON
-resultadoJSON = resultado.to_json()
+json_resultado = resultado.to_json()
 
 #mostra o JSON no terminal 
-print(resultadoJSON)
+print(json_resultado)      
+
+# Converte o JSON para objeto Python
+dados = json.loads(json_resultado)
+
+# Salva em um arquivo JSON
+with open("resultado.json", "w", encoding="utf-8") as arquivo:
+    json.dump(dados, arquivo, indent=4, ensure_ascii=False)
+
+print(f"\nImagem analisada: {imagem}")
+print("JSON salvo em: resultado.json")
